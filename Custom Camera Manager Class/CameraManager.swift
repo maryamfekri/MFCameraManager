dab186e45ff7bfb49665b11cde2c5ac85eef81d0
Fekri <m.fekri@roundtableapps.com>
2017-02-05 14:57:12 +0330

    camera manager  and sample project with camera manager

---
+++ Custom Camera Manager Class/CameraManager.swift
@@ -0,0 +1,285 @@
+//
+//  CameraManager.swift
+//  Douugh
+//
+//  Created by Fekri on 12/28/16.
+//  Copyright © 2016 RoundTableApps. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import UIKit
+
+enum CameraDevice {
+    case back
+    case front
+}
+
+class CameraManager {
+    
+    private var cameraPosition : CameraDevice?
+    private var cameraView : UIView?
+    weak private var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    //Private variables that cannot be accessed by other classes in any way.
+    fileprivate var videoDataOutput : AVCaptureStillImageOutput?
+    fileprivate var captureSession: AVCaptureSession!
+    
+    func captureSetup(in cameraView: UIView, with cameraPosition: CameraDevice? = .back) {
+        self.cameraView = cameraView
+        self.cameraPosition = cameraPosition
+        self.captureSession = AVCaptureSession()
+        switch cameraPosition! {
+        case .back:
+            self.captureSetup(AVCaptureDevicePosition.back)
+        case .front:
+            self.captureSetup(AVCaptureDevicePosition.front)
+        }
+    }
+    
+    func startRunning() {
+        if (captureSession?.isRunning != true) {
+            captureSession.startRunning()
+        }
+    }
+    
+    func stopRunning() {
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func updatePreviewFrame() {
+        if cameraView != nil {
+            self.previewLayer?.frame = cameraView!.bounds
+        }
+    }
+    
+    func transitionCamera() {
+        if let connection =  self.previewLayer?.connection  {
+            let currentDevice: UIDevice = UIDevice.current
+            let orientation: UIDeviceOrientation = currentDevice.orientation
+            
+            let previewLayerConnection : AVCaptureConnection = connection
+            
+            if (previewLayerConnection.isVideoOrientationSupported)
+            {
+                switch (orientation)
+                {
+                case .portrait:
+                    previewLayerConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+                    break
+                case .landscapeRight:
+                    previewLayerConnection.videoOrientation = AVCaptureVideoOrientation.landscapeLeft
+                    break
+                case .landscapeLeft:
+                    previewLayerConnection.videoOrientation = AVCaptureVideoOrientation.landscapeRight
+                    break
+                case .portraitUpsideDown:
+                    previewLayerConnection.videoOrientation = AVCaptureVideoOrientation.portraitUpsideDown
+                    break
+                default:
+                    previewLayerConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+                    break
+                }
+            }
+        }
+        
+    }
+    
+    func enableTorchMode(with level: Float) {
+        for testedDevice in AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo){
+            if ((testedDevice as AnyObject).position == AVCaptureDevicePosition.back && self.cameraPosition == .back) {
+                let currentDevice = testedDevice as! AVCaptureDevice
+                if currentDevice.isTorchAvailable && currentDevice.isTorchModeSupported(AVCaptureTorchMode.auto) {
+                    do {
+                        try currentDevice.lockForConfiguration()
+                        if currentDevice.isTorchActive {
+                            currentDevice.torchMode = AVCaptureTorchMode.off
+                        } else {
+                            try currentDevice.setTorchModeOnWithLevel(level)
+                        }
+                        currentDevice.unlockForConfiguration()
+                    } catch {
+                        print("torch can not be enable")
+                    }
+                }
+            }
+        }
+    }
+    
+    func getcroppedImage(with rect: CGRect, completionHandler: @escaping (UIImage?, Error?) -> Void){
+        
+        var croppedImage : UIImage?
+        if let videoConnection = videoDataOutput?.connection(withMediaType: AVMediaTypeVideo) {
+            videoDataOutput?.captureStillImageAsynchronously(from: videoConnection) {
+                (imageDataSampleBuffer, error) -> Void in
+                if imageDataSampleBuffer != nil {
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+                    
+                    // The image returned in initialImageData will be larger than what
+                    //  is shown in the AVCaptureVideoPreviewLayer, so we need to crop it.
+                    let capturedImage : UIImage = UIImage(data: imageData!)!
+                    
+                    let originalSize : CGSize
+                    let visibleLayerFrame = rect // THE ACTUAL VISIBLE AREA IN THE LAYER FRAME
+                    
+                    // Calculate the fractional size that is shown in the preview
+                    if let metaRect : CGRect = (self.previewLayer?.metadataOutputRectOfInterest(for: visibleLayerFrame)) {
+                        if (capturedImage.imageOrientation == UIImageOrientation.left || capturedImage.imageOrientation == UIImageOrientation.right) {
+                            // For these images (which are portrait), swap the size of the
+                            // image, because here the output image is actually rotated
+                            // relative to what you see on screen.
+                            originalSize = CGSize(width: capturedImage.size.height, height: capturedImage.size.width)
+                        }
+                        else {
+                            originalSize = capturedImage.size
+                        }
+                        
+                        let x = metaRect.origin.x * originalSize.width
+                        let y = metaRect.origin.y * originalSize.height
+                        // metaRect is fractional, that's why we multiply here.
+                        let cropRect : CGRect = CGRect( x: x,
+                                                        y: y,
+                                                        width: metaRect.size.width * originalSize.width,
+                                                        height: metaRect.size.height * originalSize.height).integral
+                        
+                        //getting the device orientation to change the final image orientation
+                        let imageOrientation : UIImageOrientation?
+                        let currentDevice: UIDevice = UIDevice.current
+                        let orientation: UIDeviceOrientation = currentDevice.orientation
+                        if self.cameraPosition == .back {
+                            switch (orientation) {
+                            case .portrait:
+                                imageOrientation = .right
+                            case .portraitUpsideDown:
+                                imageOrientation = .left
+                            case .landscapeRight:
+                                imageOrientation = .down
+                            case .landscapeLeft:
+                                imageOrientation = .up
+                            default:
+                                imageOrientation = .right
+                                break
+                            }
+                        } else {
+                            switch (orientation) {
+                            case .portrait:
+                                imageOrientation = .leftMirrored
+                            case .portraitUpsideDown:
+                                imageOrientation = .rightMirrored
+                            case .landscapeRight:
+                                imageOrientation = .upMirrored
+                            case .landscapeLeft:
+                                imageOrientation = .downMirrored
+                            default:
+                                imageOrientation = .leftMirrored
+                                break
+                            }
+                        }
+                        
+                        croppedImage =
+                            UIImage(cgImage: capturedImage.cgImage!.cropping(to: cropRect)!,
+                                    scale:1,
+                                    orientation: imageOrientation! )
+                        
+                        
+                        //save the original and cropped image in gallery
+//                        UIImageWriteToSavedPhotosAlbum(capturedImage, nil, nil, nil)
+//                        if croppedImage != nil {
+//                            UIImageWriteToSavedPhotosAlbum(croppedImage!, nil, nil, nil)
+//                        }
+                    }
+                }
+                completionHandler(croppedImage, error)
+            }
+        }
+        
+    }
+    
+    /**
+     this func will setup the camera and capture session and add to cameraView
+     */
+    fileprivate func captureSetup (_ position : AVCaptureDevicePosition) {
+        
+        captureSession.stopRunning()
+        captureSession = AVCaptureSession()
+        previewLayer?.removeFromSuperlayer()
+        
+        var captureError : NSError?
+        var captureDevice : AVCaptureDevice!
+        
+        //Device
+        for testedDevice in AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo){
+            if ((testedDevice as AnyObject).position == position) {
+                captureDevice = testedDevice as! AVCaptureDevice
+            }
+        }
+        if (captureDevice == nil) {
+            captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        }
+        
+        //Input
+        var deviceInput : AVCaptureDeviceInput?
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+        } catch let error as NSError {
+            captureError = error
+            deviceInput = nil
+            print("you dont have camera")
+            return
+        }
+        
+        //Output
+        self.videoDataOutput = AVCaptureStillImageOutput()
+        self.videoDataOutput?.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+        
+        if (captureError == nil) {
+            if (captureSession.canAddInput(deviceInput)) {
+                captureSession.addInput(deviceInput)
+            }
+            
+            
+            if (captureSession.canAddOutput(self.videoDataOutput)) {
+                captureSession.addOutput(self.videoDataOutput)
+            }
+        }
+        
+        //To get the highest quality of bufferData add below code
+        //        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        if cameraView != nil {
+            previewLayer?.frame = cameraView!.bounds
+        }
+        
+        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        cameraView?.layer.addSublayer(previewLayer!)
+        
+        //to detect orientation of device and to show the AVCapture as the orientation is
+        let currentDevice: UIDevice = UIDevice.current
+        let orientation: UIDeviceOrientation = currentDevice.orientation
+        let previewLayerConnection =  self.previewLayer?.connection
+        switch (orientation) {
+        case .portrait:
+            previewLayerConnection?.videoOrientation = AVCaptureVideoOrientation.portrait
+            break
+        case .landscapeRight:
+            previewLayerConnection?.videoOrientation = AVCaptureVideoOrientation.landscapeLeft
+            break
+        case .landscapeLeft:
+            previewLayerConnection?.videoOrientation = AVCaptureVideoOrientation.landscapeRight
+            break
+        case .portraitUpsideDown:
+            previewLayerConnection?.videoOrientation = AVCaptureVideoOrientation.portraitUpsideDown
+            break
+        default:
+            previewLayerConnection?.videoOrientation = AVCaptureVideoOrientation.portrait
+            break
+        }
+        
+        self.captureSession.startRunning()
+        
+    }
+    
+}
---
+++ sampleCameraManager/sampleCameraManager/Controllers/CameraViewController.swift
@@ -0,0 +1,172 @@
+//
+//  CameraViewController.swift
+//  CaptureCard
+//
+//  Created by Fekri on 12/12/16.
+//  Copyright © 2016 MaryamFekri. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    
+    @IBOutlet weak var cameraView: UIView!
+
+    var cameraManager = CameraManager()
+    var cameraPosition : CameraDevice?
+
+    let maskLayer = CALayer()
+    let rectLayer = CAShapeLayer()
+    let outlineLayer = CAShapeLayer()
+    var rectPath = UIBezierPath()
+    let useFrontTextLayer = CATextLayer()
+    let tapHereToCaptureTextLayer = CATextLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        cameraManager.startRunning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        cameraManager.stopRunning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+
+        cameraManager.transitionCamera()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.cameraManager.updatePreviewFrame()
+        drawOverRectView()
+    }
+    
+    /**
+     this func will draw a rect mask over the camera view
+     */
+    func drawOverRectView() {
+        
+        cameraView.layer.mask = nil
+        
+        let cameraSize = self.cameraView!.frame.size
+        
+        /// to calculate the height of frame based on screen size
+        var frameHeight: CGFloat = 0.0
+        /// to calculate the width of frame based on screen size
+        var frameWidth: CGFloat =  0.0
+        /// to calculate position Y of recFrame to be in center of cameraView
+        var originY: CGFloat = 0.0
+        /// to calculate position X of recFrame to be in center of cameraView
+        var originX: CGFloat = 0.0
+        
+        let currentDevice: UIDevice = UIDevice.current
+        let orientation: UIDeviceOrientation = currentDevice.orientation
+        
+        // calculatin position and frame of rectFrame based on screen size
+        switch (orientation) {
+        case .landscapeRight, .landscapeLeft:
+            frameHeight = (cameraSize.height)/1.4
+            frameWidth = cameraSize.width/1.5
+            originY = ((cameraSize.height - frameHeight)/2)
+            originX = (cameraSize.width - frameWidth)/2
+            break
+        default:
+            //if it is faceUp or portrait or any other orientation
+            frameHeight = (cameraSize.height)/1.5
+            frameWidth = cameraSize.width/1.15
+            originY = ((cameraSize.height - frameHeight)/2)
+            originX = (cameraSize.width - frameWidth)/2
+            break
+        }
+        
+        //create a rect shape layer
+        rectLayer.frame = CGRect(x: originX, y: originY, width: frameWidth, height: frameHeight)
+    
+        //create a beizier path for a rounded rectangle
+        rectPath = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: frameWidth, height: frameHeight), cornerRadius: 10)
+        
+        //add beizier to rect shapelayer
+        rectLayer.path = rectPath.cgPath
+        rectLayer.fillColor = UIColor.black.cgColor
+        rectLayer.strokeColor = UIColor.white.cgColor
+        
+        //add the outline layer
+        outlineLayer.addSublayer(rectLayer)
+        outlineLayer.frame = CGRect(x: originX, y: originY, width: frameWidth, height: frameHeight)
+        outlineLayer.path = rectPath.cgPath
+        outlineLayer.strokeColor = UIColor.white.cgColor
+        outlineLayer.fillColor = UIColor.clear.cgColor
+        outlineLayer.backgroundColor = UIColor.clear.cgColor
+        outlineLayer.lineWidth = 2
+        cameraView.layer.addSublayer(outlineLayer)
+        
+        useFrontTextLayer.fontSize = 13
+        useFrontTextLayer.foregroundColor = UIColor.white.cgColor
+        useFrontTextLayer.alignmentMode = kCAAlignmentCenter
+        useFrontTextLayer.frame = CGRect(origin: outlineLayer.frame.origin, size: CGSize(width: 100, height: 30))
+        useFrontTextLayer.position = CGPoint(x: (frameWidth/2), y: 24)
+        useFrontTextLayer.contentsScale = UIScreen.main.scale
+        outlineLayer.addSublayer(useFrontTextLayer)
+        
+        //add shapelayer to layer
+        maskLayer.frame = cameraView.bounds
+        maskLayer.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5).cgColor
+        maskLayer.addSublayer(rectLayer)
+        
+        //add layer mask to camera view
+        cameraView.layer.mask = maskLayer
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier, identifier == "CameraView2ImageView" {
+            (segue.destination as? ImageViewController)?.image = sender as? UIImage
+        }
+    }
+    
+    //MARK: - Actions
+    @IBAction func useFlashButtonTouchedUpInside(_ sender: UIButton) {
+        captureAndCropp()
+//        cameraManager.enableTorchMode(with: 1)
+    }
+    
+    @IBAction func tapGestureAction(_ sender: Any) {
+        captureAndCropp()
+    }
+    
+    func captureAndCropp() {
+        self.cameraManager.getcroppedImage(with: self.outlineLayer.frame) {
+            (croppedImage, error) -> Void in
+            OperationQueue.main.addOperation({
+                if croppedImage != nil {
+                    self.performSegue(withIdentifier: "CameraView2ImageView", sender: croppedImage)
+                }
+            })
+        }
+    }
+}
+
+// MARK: - InitView
+extension CameraViewController {
+    func initView() {
+        
+        self.cameraPosition = .back
+        cameraManager.captureSetup(in: self.cameraView, with: .back)
+
+    }
+}
+
+
